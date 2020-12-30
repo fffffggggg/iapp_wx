d788bb34dc75f217ba4730cfc7481628e77d81f0
FearlessMa <458871138@qq.com>
2020-12-30 18:46:30 +0800

    docs: 修改方法注释

--- src/service/index.ts
+++ src/service/index.ts
@@ -14,15 +14,16 @@ interface IFetchData {
 
 
 
+type methodsType = "OPTIONS" | "POST" | "GET" | "HEAD" | "PUT" | "DELETE" | "TRACE" | "CONNECT";
 /**
  * 默认请求封装
  *
- * @param {*} path api 地址
- * @param {*} params 请求参数
+ * @param {string} path api 地址
+ * @param {{ [name: string]: any }} [params={}] 请求参数
+ * @param {methodsType} [method="GET"] 默认方法GET
  * @param {request.Option} [option] taro.request.option的配置
+ * @returns {RequestTask<any>} taro.request
  */
-
-type methodsType = "OPTIONS" | "POST" | "GET" | "HEAD" | "PUT" | "DELETE" | "TRACE" | "CONNECT";
 const defaultRequest = (path: string, params: { [name: string]: any } = {}, method: methodsType = "GET", option?: request.Option): RequestTask<any> => {
   const url: string = domain + api[path];
   const defaultOption: request.Option = { url, method, data: params };
